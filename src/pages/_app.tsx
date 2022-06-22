6b08147a3ba2c529d9bce50c96c63d3587a4bd4c
elenh <yisiwings@163.com>
2022-06-22 18:55:10 +0800

    feat: support layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import '~/styles/globals.css'
+import LayoutDefault from '~/layouts/default'
 import type { AppProps } from 'next/app'
-import DefaultLayout from '~/layouts/default'
+import type { NextPage } from 'next'
+import type { ReactElement, ReactNode } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
-  )
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // 如果 page 级别的组件定义了 getLayout 则表示该页面有自定义 layout
+  // 否则，使用默认 layout
+
+  const getLayout =
+    Component.getLayout || ((page) => <LayoutDefault>{page}</LayoutDefault>)
+
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
-
 import getConfig from 'next/config'
 import { useEffect } from 'react'
+import styles from '~/styles/Home.module.css'
 
 const { publicRuntimeConfig } = getConfig()
 
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
     console.log('runtime config:', publicRuntimeConfig)
   }, [])
 
-  return <div>ya hoo!</div>
+  return <div className={styles.container}>ya hoo! this is home page!!!</div>
 }
 
 export default Home
